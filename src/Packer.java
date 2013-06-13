9813dab542f5c9c700a2a141250a0d8b0ed87149
drinor <drinor2@gmail.com>
2013-06-13 19:02:24 +0200

    textures-packer project.

---
+++ src/Packer.java
@@ -0,0 +1,14 @@
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
+
+public class Packer
+{
+	public static void main(String[] args) 
+	{
+		   Settings settings = new Settings();
+           settings.maxWidth = 512;
+           settings.maxHeight = 512;
+           settings.flattenPaths = true;
+           TexturePacker2.process(settings, "images", "data", "game");
+	}
+}
