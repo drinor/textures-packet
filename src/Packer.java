ce20cee3cc3e76face46126bd60aaed5095cd7be
Rafael Jurado González <rjurado@nosolosoftware.biz>
2013-07-04 23:24:02 +0200

    Corrected indexation.

--- src/Packer.java
+++ src/Packer.java
@@ -4,11 +4,11 @@ import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 public class Packer
 {
 	public static void main(String[] args) 
-	{
-		   Settings settings = new Settings();
-           settings.maxWidth = 512;
-           settings.maxHeight = 512;
-           settings.flattenPaths = true;
-           TexturePacker2.process(settings, "images", "data", "game");
+	{ 
+		Settings settings = new Settings();
+		settings.maxWidth = 512;
+	        settings.maxHeight = 512;
+        	settings.flattenPaths = true;
+           	TexturePacker2.process(settings, "images", "data", "game");
 	}
 }
